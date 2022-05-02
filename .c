46d66d227ff69f042d5478b931b87c0eec5c7856
flavio <94140420+ndbzika@users.noreply.github.com>
2022-05-02 11:06:03 -0300

    Delete .c

--- .c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main(){
-	float numero1,numero2,numero3;
-	printf("Informe os tres numeros:");
-	scanf("%f%f%f", &numero1,&numero2,&numero3);
-	float media = (numero1+numero2+numero3)/3;
-	printf("A media aritmetica dos numeros informados eh %.2f",media);
-	getch();
-}
